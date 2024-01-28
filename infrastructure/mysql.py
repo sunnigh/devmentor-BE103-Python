b8213be8338b26cebcb98925febcf99eca4310d5
Jonny <ji394su311203106@gmail.com>
2024-01-28 22:41:36 +0800

    chore: connect mysql with driver

--- infrastructure/mysql.py
+++ infrastructure/mysql.py
@@ -2,10 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql://admin:1234@localhost:3306/be103"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://admin:1234@localhost:3306/be103"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, echo=True,
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
