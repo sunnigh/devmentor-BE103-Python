61e7b12a279ebee9617fbc77fdc4a922e19d2f5d
Jonny <ji394su311203106@gmail.com>
2024-01-28 18:20:17 +0800

    feat: remove delete put

--- api/v1/post.py
+++ api/v1/post.py
@@ -15,12 +15,3 @@ def list_post():
 def create_post():
     return {"status": "ok"}
 
-
-@router.delete("/{post_id}")
-def delete_post(post_id: str):
-    return {"status": "ok"}
-
-
-@router.put("/{post_id}")
-def update_post(post_id: str):
-    return {"status": "ok"}
