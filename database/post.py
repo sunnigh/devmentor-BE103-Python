0a45c3f79a8b6e6518746d8c1cc5af73572598ab
Jonny <ji394su311203106@gmail.com>
2024-01-28 22:50:28 +0800

    feat: update models post

--- database/post.py
+++ database/post.py
@@ -7,8 +7,8 @@ class Post(Base):
     __tablename__ = "posts"
 
     id = Column(Integer, primary_key=True)
-    title = Column(String, index=True)
-    content = Column(String)
+    title = Column(String(255), index=True)
+    content = Column(String(255))
     is_active = Column(Boolean, default=True)
 
 
