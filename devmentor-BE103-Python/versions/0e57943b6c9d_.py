9d7383c4a06adb171d67d8c5367429824a5e83c8
sunnigh <d7031887@gmail.com>
2024-06-13 17:24:58 +0800

    add_student_column

---
+++ devmentor-BE103-Python/versions/0e57943b6c9d_.py
@@ -0,0 +1,31 @@
+"""empty message
+
+Revision ID: 0e57943b6c9d
+Revises: ff3c26ae043a
+Create Date: 2024-06-13 17:19:33.345638
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '0e57943b6c9d'
+down_revision: Union[str, None] = 'ff3c26ae043a'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "posts",
+        sa.Column("student", sa.String(length=255), nullable=False),
+
+    )
+    pass
+
+
+def downgrade() -> None:
+    pass
