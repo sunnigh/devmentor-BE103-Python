6840de1c56e2a99c82604fc6bc8dc258e04ae826
sunnigh <d7031887@gmail.com>
2024-06-24 22:32:47 +0800

    feature/event_setting

---
+++ api/v1/event.py
@@ -0,0 +1,46 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+import repository.event
+from infrastructure.mysql import get_db
+from schema.database.event import EventCreate, EventUpdate
+
+router = APIRouter(
+    tags=["event"],
+    prefix="/events"
+)
+
+# 取得全部
+# [GET] /v1/events
+@router.get("/")
+def list_event(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    events = repository.event.lists(db, skip=skip, limit=limit)
+    return events
+
+# 新增
+# [POST] /v1/events
+@router.post("/")
+def create_event(event: EventCreate, db: Session = Depends(get_db)):
+    return repository.event.create(db=db, event=event)
+
+# 取得特定
+# [GET] /v1/events/{event_id}
+@router.get("/{event_id}")
+def get_event(event_id: int, db: Session = Depends(get_db)):
+    event = repository.event.get(db, event_id)
+    return event
+
+# 更新特定
+# [PUT] /v1/events/{event_id}
+@router.put("/{event_id}")
+def update_event(event_id: int, event_update: EventUpdate, db: Session = Depends(get_db)):
+    event = repository.event.update(db, event_id, event_update)
+
+    return event
+
+# 刪除特定
+# [DELETE] /v1/events/{event_id}
+@router.delete("/{event_id}")
+def delete_event(event_id: int, db: Session = Depends(get_db)):
+    event = repository.event.delete(db, event_id)
+    return event
--- api/v1/routes.py
+++ api/v1/routes.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
 from api.v1.post import router as post_router
+from api.v1.event import router as event_router
 
 
 routers = APIRouter()
 router_list = [
-    post_router
+    post_router,
+    event_router,
 ]
 
 for router in router_list:
