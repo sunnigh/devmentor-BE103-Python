8f058763d80c3fddc613feb5660d55a6d211168f
sunnigh <d7031887@gmail.com>
2024-07-18 21:36:52 +0800

    feature/table_upgrade

--- devmentor-BE103-Python/versions/ba02b944b5f2_usertable_islogin_type_change.py
+++ devmentor-BE103-Python/versions/ba02b944b5f2_usertable_islogin_type_change.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'ba02b944b5f2'
-down_revision: Union[str, None] = 'ee5c493f0816'
+down_revision: Union[str, None] = '5c22fd3d24ce'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
