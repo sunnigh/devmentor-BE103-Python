da897c23126152cb28bfbadf730ab48658721020
sunnigh <d7031887@gmail.com>
2024-07-19 14:43:25 +0800

    feature/notifyservice_get&post

--- database/user.py
+++ database/user.py
@@ -6,11 +6,11 @@ from sqlalchemy.orm import relationship
 class User(Base):
     __tablename__ = "users"
 
-    user_id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
     user_name = Column(String(255), nullable=False)
     account = Column(String(255), unique=True, nullable=False)
     password = Column(String(255), nullable=False)
     is_login = Column(Boolean, nullable=False,default=False)
     language = Column(String(255), nullable=False)
 
-    subscriptions = relationship("Subscribe", back_populates="user")
+    # subscriptions = relationship("Subscribe", back_populates="user")
