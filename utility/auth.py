01d5cf07bec324d8f0fd48b529155f24de726699
sunnigh <d7031887@gmail.com>
2024-07-24 19:34:12 +0800

    feature/trigger_send_email

--- utility/auth.py
+++ utility/auth.py
@@ -1,6 +1,8 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 from passlib.context import CryptContext
-from sqlalchemy.orm import Session
-from database.user import User
+
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -22,6 +24,30 @@ def verify_password(plain_password, hashed_password):
     :return:
     """
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def send_email(to_address: str, username: str, content_data: str):
+    from_address = "my_email_adress"
+    password = "jdvxjaokltuhoshx"
+
+    msg = MIMEMultipart()
+    msg['From'] = from_address
+    msg['To'] = to_address
+    msg['Subject'] = "event_email"
+
+    body = f"Hi {username},\n\nThis is an information for you.\n\n{content_data}\n\nBest regards,"
+    msg.attach(MIMEText(body, 'plain'))
+
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(from_address, password)
+        text = msg.as_string()
+        server.sendmail(from_address, to_address, text)
+        server.quit()
+        print(f"Email sent successfully to {to_address}")
+    except Exception as e:
+        print(f"Failed to send email to {to_address}. Error: {str(e)}")
 
 
 
